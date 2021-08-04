a305752aea25ff3c3acbf7ac7861f33879c053f1
ovunc.girgin <ovunc.girgin@gmail.com>
2021-08-04 14:10:32 +0300

    Update JobAdv.jsx

--- src/pages/JobAdv.jsx
+++ src/pages/JobAdv.jsx
@@ -1,41 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { Item, Label } from "semantic-ui-react";
+import { Item, Label, Header, Icon } from "semantic-ui-react";
 import { Container } from "semantic-ui-react";
 import JobAdvertisementServices from "../services/jobAdvertisement";
 
 export default function JobAdv() {
   const [jobs, setjobs] = useState([]);
 
-  useEffect(() =>{
-      let jobAdvService = new JobAdvertisementServices()
+  useEffect(() => {
+    let jobAdvService = new JobAdvertisementServices();
 
-      jobAdvService.getAllAdvertisements().then(result=>setjobs(result.data.data))
-  },[])
+    jobAdvService
+      .getAllAdvertisements()
+      .then((result) => setjobs(result.data.data));
+  }, []);
 
   return (
     <div>
-      <Container>
+      <Container style={{ marginTop: "1cm" }}>
         <Item.Group divided>
-          { jobs.map(job => (
-            <Item>
+          {jobs.map((job) => (
+            <Item style={{ marginTop: "1cm" }}>
               <Item.Image
                 style={{ marginLeft: "11cm" }}
-                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                src="https://www.thoughtco.com/thmb/r36ve3S3mJuL0fvdNbVcXRJ0SjA=/774x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/what-is-java-5b4bda1cc9e77c0037171617.jpg"
               />
               <Item.Content>
-                <Item.Header>{job.jobPositions.title}</Item.Header>
+                <Item.Header>
+                  <Header as="h3" >
+                    <Header.Content>{job.jobPositions.title}</Header.Content>
+                  </Header>
+                </Item.Header>
                 <Item.Meta>
-                  <span className="cinema">{job.cities.city}</span>
+                  <span className="cinema">{job.employers.companyName}</span>
                 </Item.Meta>
                 <Item.Description></Item.Description>
                 <Item.Extra>
-                  <Label>IMAX</Label>
-                  <Label icon="globe" content="Additional Languages" />
+                  <Label>{job.cities.city}</Label>
+                  <Label content={job.workingType} />
                 </Item.Extra>
               </Item.Content>
             </Item>
-          ))
-          }
+          ))}
         </Item.Group>
       </Container>
     </div>
